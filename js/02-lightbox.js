98fbb77babfe96d257c623c32213ab80b6c609c6
KlassArtem <1trabl123@gmail.com>
2022-12-30 18:15:12 +0200

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+import { galleryItems } from "./gallery-items.js";
+
+const ref = {
+  gallery: document.querySelector(".gallery"),
+};
+
+function createGallaryElementMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li>
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    </li>
+    `;
+    })
+    .join("");
+}
+
+const gallaryMarkup = createGallaryElementMarkup(galleryItems);
+
+ref.gallery.innerHTML = gallaryMarkup;
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
