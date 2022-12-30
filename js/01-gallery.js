a3deaf5fdba926a202628ed247f0e6faf1b8991c
KlassArtem <111056786+KlassArtem@users.noreply.github.com>
2022-12-30 17:38:14 +0200

    added 1rst task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkUp = makeGalleryMarkUp(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkUp)
+galleryContainer.addEventListener('click', openInstanceModal);
+
+function makeGalleryMarkUp(pictures) {
+    return pictures
+      .map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+      })
+      .join('');
+  }
+
+  const instance = basicLightbox.create(
+    `
+        <img src="" />
+    `,
+    {
+      onShow: () => {
+        console.log('add listener ');
+        document.addEventListener('keydown', escBtnHandler);
+      },
+      onClose: () => {
+        console.log('remove listener ');
+        document.removeEventListener('keydown', escBtnHandler);
+      },
+    }
+  );
+  
+  function escBtnHandler(e) {
+    if (e.code === 'Escape') {
+      instance.close();
+    }
+  }
+  
+  function openInstanceModal(e) {
+    e.preventDefault();
+    instance.element().querySelector('img').src = e.target.dataset.source;
+    instance.show();
+  }
+
